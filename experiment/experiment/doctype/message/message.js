dc8376db76be01fcfcdb66e823686568f57b0f1a
Saif Sallam <saif11sallam@gmail.com>
2025-08-07 12:16:04 +0300

    changed the logic but still didn't work

--- experiment/experiment/doctype/message/message.js
+++ experiment/experiment/doctype/message/message.js
@@ -1,8 +1,21 @@
-// Copyright (c) 2025, saif and contributors
-// For license information, please see license.txt
+// message.js
 
-// frappe.ui.form.on("Message", {
-// 	refresh(frm) {
+frappe.ui.form.on("Message", {
+    validate(frm) {
+        if (frm.doc.use_indicator) {
+            frappe.msgprint({
+                title: "Indicator Check",
+                message: "The 'Use Indicator' field is checked.",
+                indicator: frm.doc.indicator_color,
+                primary_action: {
+                    label: "Go to Google",
+                    action() {
+                        window.open("https://www.google.com", "_blank");
+                    }
+                }
+            });
 
-// 	},
-// });
+            frappe.validated = false; // stop form from saving
+        }
+    }
+});
--- experiment/experiment/doctype/message/message.py
+++ experiment/experiment/doctype/message/message.py
@@ -1,42 +1,19 @@
-# Copyright (c) 2025, saif and contributors
-# For license information, please see license.txt
+# message.py
 
 import frappe
 from frappe.model.document import Document
 
-
 class Message(Document):
-	
-	def validate(self):
+    def validate(self):
+        if not self.message_text:
+            frappe.msgprint(
+                "The message text must have a value",
+                title="Error",
+                indicator="orange",
+            )
 
-		if not self.message_text:
-			frappe.msgprint(
-				"the messge text must have value",
-				title="Error",
-				indicator="orange",
-				#raise_exception=True
-			)
-		
-		if self.use_indicator:
-			frappe.msgprint(
-				"the use indicator must not be checked",
-				indicator=self.indicator_color,
-				#raise_exception=True
-			)
-
-		if self.list_data:
-					# We split the string by comma to get a Python list
-					list_items = [item.strip() for item in self.list_data.split(',') if item.strip()]
-
-					frappe.msgprint(
-						# The message itself is now a list
-						list_items,
-						title="Title",
-						indicator="green",
-						as_list=True,
-						primary_action={
-							'label': "Go to Google",
-							# The 'action' key is a string containing valid JavaScript
-							'action': "frappe.open_url('https://www.google.com')"
-						}
-					)
+        if self.use_indicator:
+            frappe.msgprint(
+                "The 'Use Indicator' field must not be checked",
+                indicator=self.indicator_color,
+            )
